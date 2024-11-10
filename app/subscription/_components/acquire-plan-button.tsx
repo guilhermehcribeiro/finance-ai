238d3e4cf5a6a0842be49819582afbecbd344b89
Guilherme Ribeiro <g.ribeiro@ligadaweb.com.br>
2024-11-10 09:39:21 -0300

    feat: stripe checkout

---
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Button } from "@/app/_components/ui/button";
+import { createStripeCheckout } from "../_actions/create-stripe-checkout";
+
+const AcquirePlanButton = () => {
+  const handleAcquirePlanClick = async () => {
+    const { sessionId } = await createStripeCheckout();
+
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Missing Stripe publishable key");
+    }
+
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+
+    await stripe.redirectToCheckout({ sessionId });
+  };
+  return (
+    <Button
+      className="w-full rounded-full font-bold"
+      onClick={handleAcquirePlanClick}
+    >
+      Adquirir plano
+    </Button>
+  );
+};
+
+export default AcquirePlanButton;
