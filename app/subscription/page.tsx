a91fc649fe08ffd66bc8cb2e0b6b007e5c972a99
Guilherme Ribeiro <g.ribeiro@ligadaweb.com.br>
2024-11-10 12:45:41 -0300

    feat: add stripe webhooks subscriptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { CheckIcon, XIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import NavBar from "../_components/navbar";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import AcquirePlanButton from "./_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -11,6 +12,8 @@ const SubscriptionPage = async () => {
     redirect("/login");
   }
 
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user?.publicMetadata?.subscriptionPlan === "premium";
   return (
     <>
       <NavBar />
@@ -42,7 +45,12 @@ const SubscriptionPage = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid">
+            <CardHeader className="relative border-b border-solid">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
